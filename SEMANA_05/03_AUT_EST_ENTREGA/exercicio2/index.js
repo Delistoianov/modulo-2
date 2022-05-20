9399eaf7f0cedd56444f5846d50662b2c3370781
Delistoianov <joao.gonzalez@sou.inteli.edu.br>
2022-05-20 14:50:50 -0300

    Exercícios de programação

--- SEMANA_05/03_AUT_EST_ENTREGA/exercicio2/index.js
+++ SEMANA_05/03_AUT_EST_ENTREGA/exercicio2/index.js
@@ -1,17 +1,24 @@
-function calc() {
-    var consulta = document.querySelector("#preço")
-    var c1 = Number(consulta.value)
-    var div1 = parseInt(c1 / 100)
-    var div2 = parseInt(((c1 % 100) % 100) / 50)
-    var div3 = parseInt(((c1 % 100) % 50) / 20)
-    var div4 = parseInt(((c1 % 100) % 20) / 10)
-    var div5 = parseInt(((c1 % 100) % 10) / 5)
-    var div6 = parseInt(((c1 % 100) % 5) / 2)
-    var div7 = parseInt(((c1 % 100) % 2) / 1)
-    console.log(div7)
-    document.getElementById('hj').textContent = div7
+function calcular() {
+    var input = document.querySelector("preco").value
+    nota100 = parseInt(input / 100);
+    nota50 = parseInt((input % 100) / 50)
+    nota20 = parseInt(((input % 100) % 50) / 20)
+    nota10 = parseInt((((input % 100) % 50) % 20) / 10)
+    nota5 = parseInt(((((input % 100) % 50) % 20) % 10) / 5)
+    nota2 = parseInt((((((input % 100) % 50) % 20) % 10) % 5) / 2)
+    nota1 = parseInt((((((input % 100) % 50) % 20) % 10) % 5) % 2)
 
+    document.querySelector("t100").innerHTML = nota100 + " notas de R$";
 
+    document.querySelector("t50").innerHTML = nota50 + " notas de R$";
 
+    document.querySelector("t120").innerHTML = nota20 + " notas de R$";
+
+    document.querySelector("t10").innerHTML = nota10 + " notas de R$";
+
+    document.querySelector("t5").innerHTML = nota5 + " notas de R$";
+
+    document.querySelector("t2").innerHTML = nota2 + " notas de R$";
+
+    document.querySelector("t1").innerHTML = nota1 + " notas de R$";
 }
-
